69fe4a3f6c360a02f6a08d9645d10471a8368389
Dave Hay <davehay93@gmail.com>
2023-03-15 14:58:02 -0700

    queue works

--- src/queue/queue.rs
+++ src/queue/queue.rs
@@ -7,6 +7,10 @@ impl<T> Queue<T> {
         Queue { items: Vec::new() }
     }
 
+    pub fn size(&self) -> usize {
+        self.items.len()
+    }
+
     pub fn enqueue(&mut self, item: T) {
         self.items.push(item)
     }
@@ -15,7 +19,42 @@ impl<T> Queue<T> {
         if self.items.len() == 0 {
             None
         } else {
-            Some(self.items.swap_remove(0))
+            Some(self.items.remove(0))
         }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        return self.size() == 0;
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        self.items.first()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_enqueue() {
+        let mut q = Queue::<i32>::new();
+        q.enqueue(1);
+        q.enqueue(2);
+        assert_eq!(q.size(), 2);
+        assert_eq!(q.front(), Some(&1));
+    }
+
+    #[test]
+    fn test_dequeue() {
+        let mut q = Queue::<i32>::new();
+        q.enqueue(1);
+        q.enqueue(2);
+        assert_eq!(q.dequeue(), Some(1));
+        assert_eq!(q.size(), 1);
+        assert_eq!(q.front(), Some(&2));
+        assert_eq!(q.dequeue(), Some(2));
+        assert_eq!(q.size(), 0);
+        assert_eq!(q.front(), (None));
     }
 }
