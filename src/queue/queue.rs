e28568b85aaa41796f2e8c3aa9e62c9155ebae6a
Dave Hay <davehay93@gmail.com>
2023-03-15 14:44:22 -0700

    vec q

--- src/queue/queue.rs
+++ src/queue/queue.rs
@@ -1,36 +1,9 @@
-struct Node<T> {
-    item: Option<T>,
-    next: Option<Box<Node<T>>>,
-}
-
-impl<T> Node<T> {
-    fn new(item: T) -> Self {
-        Node { Some(item), next: None }
-    }
-}
-
 pub struct Queue<T> {
-    first: Option<Node<T>>,
-    last: Option<Node<T>>,
-    n: u32,
+    items: Vec<T>,
 }
 
 impl<T> Queue<T> {
     pub fn new() -> Self {
-        let mut dummy1 = Node::new(None);
-        let mut dummy2 = Node::new(None);
-        Queue {
-            first: dummy1,
-            last: dummy2,
-            n: 1,
-        }
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.n == 0
-    }
-
-    pub fn size(&self) -> u32 {
-        self.n
+        Queue { items: Vec::new() }
     }
 }
