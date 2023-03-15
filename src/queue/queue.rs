7ea9acc37d74f0e23b010bb49d5418f4603a6a7d
Dave Hay <davehay93@gmail.com>
2023-03-15 14:52:25 -0700

    think it works

--- src/queue/queue.rs
+++ src/queue/queue.rs
@@ -6,4 +6,16 @@ impl<T> Queue<T> {
     pub fn new() -> Self {
         Queue { items: Vec::new() }
     }
+
+    pub fn enqueue(&mut self, item: T) {
+        self.items.push(item)
+    }
+
+    pub fn dequeue(&mut self) -> Option<T> {
+        if self.items.len() == 0 {
+            None
+        } else {
+            Some(self.items.swap_remove(0))
+        }
+    }
 }
