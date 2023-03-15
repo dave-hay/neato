c748903a8a07b24bd05ce1f1a3ba52c12af2854b
Dave Hay <davehay93@gmail.com>
2023-03-15 14:42:48 -0700

    confuse

--- src/queue/queue.rs
+++ src/queue/queue.rs
@@ -1,9 +1,36 @@
+struct Node<T> {
+    item: Option<T>,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(item: T) -> Self {
+        Node { Some(item), next: None }
+    }
+}
+
 pub struct Queue<T> {
-    items: Vec<T>,
+    first: Option<Node<T>>,
+    last: Option<Node<T>>,
+    n: u32,
 }
 
 impl<T> Queue<T> {
     pub fn new() -> Self {
-        Queue { items: Vec::new() }
+        let mut dummy1 = Node::new(None);
+        let mut dummy2 = Node::new(None);
+        Queue {
+            first: dummy1,
+            last: dummy2,
+            n: 1,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.n == 0
+    }
+
+    pub fn size(&self) -> u32 {
+        self.n
     }
 }
